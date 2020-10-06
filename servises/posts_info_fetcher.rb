226c14c86c736ac6404b07d887bf4bfcb4d790de
igor.patreew <igor.patreew@activeplatform.com>
2020-10-06 14:58:18 +0300

    Add SQL.squish

--- servises/posts_info_fetcher.rb
+++ servises/posts_info_fetcher.rb
@@ -1,33 +1,38 @@
-# require 'pry'
+require 'pry'
 require 'sinatra/reloader'
-# require './servises/connection.rb'
 
 class PostsInfoFetcher < InfoFetcher
   def average_post_rating(post_id)
-    connection.exec("SELECT posts.title, AVG(estimations.value)
-                     FROM estimations
-                     JOIN posts
-                     ON posts.id = estimations.post_id
-                     WHERE posts.id = #{post_id}
-                     GROUP BY posts.title;")
+    connection.exec(<<-SQL.squish)
+      SELECT posts.title, AVG(estimations.value)
+      FROM estimations
+      JOIN posts
+      ON posts.id = estimations.post_id
+      WHERE posts.id = #{post_id}
+      GROUP BY posts.title
+    SQL
   end
 
-  def best_posts_post_rating_list(n)
-    connection.exec("SELECT posts.title, AVG(estimations.value)
-                     FROM estimations
-                     JOIN posts ON posts.id = estimations.post_id
-                     GROUP BY posts.title
-                     ORDER BY AVG(estimations.value)
-                     DESC LIMIT #{n};")
+  def best_posts_post_rating_list(num)
+    connection.exec(<<-SQL.squish)
+      SELECT posts.title, AVG(estimations.value)
+      FROM estimations
+      JOIN posts ON posts.id = estimations.post_id
+      GROUP BY posts.title
+      ORDER BY AVG(estimations.value)
+      DESC LIMIT #{num}
+    SQL
   end
 
   def ip_list
-    connection.exec("SELECT posts.ip_address,
-                     STRING_AGG(DISTINCT users.login, ';')
-                     FROM posts
-                     JOIN users
-                     ON posts.user_id = users.id
-                     GROUP BY posts.ip_address
-                     HAVING count(DISTINCT users.login) > 1;")
+    connection.exec(<<-SQL.squish)
+      SELECT posts.ip_address,
+      STRING_AGG(DISTINCT users.login, ';')
+      FROM posts
+      JOIN users
+      ON posts.user_id = users.id
+      GROUP BY posts.ip_address
+      HAVING count(DISTINCT users.login) > 1
+    SQL
   end
 end
